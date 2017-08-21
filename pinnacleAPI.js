aa7d5cf865209773f42242c68a5dc9109e706738
nomad <sbefort@gmail.com>
2017-08-20 20:26:21 -0500

    Moving from request to axios

--- pinnacleAPI.js
+++ pinnacleAPI.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var _ = require('lodash');
-var request = require('request');
+var axios = require('axios');
 var qs = require('qs');
 var operations = require('./pinnacleOperations');
 
 var PinnacleAPI = function(username, password) {
-	if(!username || !password){
+	if (!username || !password){
 		throw new Error('No username and/or password provided in createClient().');
 	}
-	var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+	// var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
 
 	var setOperation = (function(operation){
 		this.operation = operation;
@@ -18,8 +18,7 @@ var PinnacleAPI = function(username, password) {
 	var buildUrl = (function() {
 		if (this.environment === 'development') {
 			this.url = operations[this.operation].devUrl;
-		}
-		else {
+		} else {
 			this.url = 'https://api.pinnacle.com/' + operations[this.operation].version + '/' + operations[this.operation].endpoint;
 		}
 	}).bind(this);
@@ -35,86 +34,88 @@ var PinnacleAPI = function(username, password) {
 		}
 	}).bind(this);
 
-	var get = (function(options, cb) {
-		var requestOptions = {
-			url: this.url + '?' + qs.stringify(options),
-			rejectUnauthorized: false,
-			headers: {
-				'Authorization': auth
-			}
-		};
-		request.get(requestOptions, (function (err, response, body) {
-			if (err) return cb(err);
-			if (!body || _.isEmpty(JSON.parse(body))) return cb(null, '');
-			parse(body, function(err, parsed) {
-				if (err) return cb(err);
-				cb(null, response, parsed);
-			});
-		}).bind(this));
-	}).bind(this);
-
 	var parse = function(body, cb) {
 		try {
 			var result = JSON.parse(body);
 			return cb(null, result);
-		}
-		catch(e){
+		} catch (e) {
 			return cb(e + ', Data: ' + body);
 		}
-	}
+	};
+
+	var get = ((options, cb) => {
+		var url = this.url + '?' + qs.stringify(options);
+		var axiosConfig = {
+			auth: {
+				username,
+				password
+			}
+		};
+		axios.get(url, axiosConfig)
+		.then((response) => {
+			if (!response.data || _.isEmpty(JSON.parse(response.data))) return cb(null, '');
+			parse(response.data, (err, parsed) => {
+				if (err) return cb(err);
+				cb(null, parsed);
+			});
+		})
+		.catch((err) => {
+			return cb(err);
+		});
+	});
 
 	this.setEnvironmentDev = function(){
 		this.environment = 'development';
-	}
+	};
 
 	this.getSports = function(cb) {
 		var options = {};
 		setOperation('getSports');
 		buildUrl();
 		get(options, cb);
-	}
+	};
 
 	this.getLeagues = function(options, cb) {
 		setOperation('getLeagues');
 		buildUrl();
 		checkRequired(options, cb);
 		get(options, cb);
-	}
+	};
 
 	this.getFixtures = function(options, cb) {
 		setOperation('getFixtures');
 		buildUrl();
 		checkRequired(options, cb);
 		get(options, cb);
-	}
+	};
 
 	this.getSettledFixtures = function(options, cb) {
 		this.operation = 'getSettledFixtures';
 		buildUrl();
 		checkRequired(options, cb);
 		get(options, cb);
-	}
+	};
 
 	this.getTeaserGroups = function(options, cb) {
 		this.operation = 'getTeaserGroups';
 		buildUrl();
 		checkRequired(options, cb);
 		get(options, cb);
-	}
+	};
 
 	this.getOdds = function(options, cb) {
 		this.operation = 'getOdds';
 		buildUrl();
 		checkRequired(options, cb);
 		get(options, cb);
-	}
+	};
 
 	this.getCurrencies = function(options, cb) {
 		this.operation = 'getCurrencies';
 		buildUrl();
 		checkRequired(options, cb);
 		get(options, cb);
-	}
-}
+	};
+};
 
 module.exports = PinnacleAPI;
